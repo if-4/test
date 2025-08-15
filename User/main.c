e741dee34caa6a570a4e6436f40aaf88b332e6c0
hit-xiaolian <xiaolian20002018@gmail.com>
2025-08-15 15:15:55 +0800

    我又瞎写的

--- User/main.c
+++ User/main.c
@@ -22,7 +22,7 @@ int main(void)
 	while (1)
 	{
 		OLED_ShowNum(1, 5, AD_Value[0], 4);		//显示转换结果第0个数据啊啊
-		OLED_ShowNum(2, 5, AD_Value[1], 4);		//显示转换结果第1个数据
+		OLED_ShowNum(2, 5, AD_Value[1], 4);		//显示转换结果第1个数据啊啊啊啊
 		OLED_ShowNum(3, 5, AD_Value[2], 4);		//显示转换结果第2个数据
 		OLED_ShowNum(4, 5, AD_Value[3], 4);		//显示转换结果第3个数据
 		
